9143b6e7c933b563d08c2f28e8154553064c40b9
skandarani <33575347+skandarani@users.noreply.github.com>
2018-10-06 14:52:25 +0100

    first-commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function page (){
+alert("This Page Has benn download");
+}
+setTimeout(page,3000);//serInterval
+var a = prompt("what's You Name ?");
+confirm("Are You Sure ?");
+alert("Welcome "+a);
+var element = document.querySelector('#faza').innerHTML="dem1 nakra 10:30";
+var element =document.querySelector('#saha').setAttribute("src","https://www.monchien.info/wp-content/uploads/2018/02/jouer-avec-son-chien.jpg");
+var element = document.querySelector('#faza').style.fontSize = "40px";
+var element = document.querySelector('#hedha').style.backgroundColor = "yellow";
+
+$('h2').hide();
